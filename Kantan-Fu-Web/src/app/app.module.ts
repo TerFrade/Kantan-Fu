c639c650cd502e07dca8c3028db54162f9d8f79c
Terence <TerenceFrade@belgiumcampus.ac.za>
2019-07-16 11:56:58 +0200

    Improved First Design

--- Kantan-Fu-Web/src/app/app.module.ts
+++ Kantan-Fu-Web/src/app/app.module.ts
@@ -1,14 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Router } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+// Services 
+// Material Design Components -> imports
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
 
+// Components -> imports
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+import { LessonComponent } from './lesson/lesson.component';
+import { TopicComponent } from './topic/topic.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, TestComponent, LessonComponent, TopicComponent],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatMenuModule,
+
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'tests', component: TestComponent },
+      { path: 'lessons', component: LessonComponent },
+      { path: 'topics', component: TopicComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
